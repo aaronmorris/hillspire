9bc1c722fbb2760de038af1f15cf965b4bea2d28
Aaron Morris <aaronmorris@users.noreply.github.com>
2023-04-14 09:06:57 -0700

    Update utilities.js

--- force-app/main/default/lwc/utilities/utilities.js
+++ force-app/main/default/lwc/utilities/utilities.js
@@ -2,6 +2,7 @@ import { LightningElement } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export const showToast = (title, message, variant, mode) => {
+  // TODO: Update message to be able to handle error objects as well
   dispatchEvent(new ShowToastEvent({
     'title': title,
     'message': message,
